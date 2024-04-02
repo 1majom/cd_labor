0bd8cddf2554e21b1f8d43041902c3ee3e87fbe0
szebala <szebala@sch.bme.hu>
2024-04-02 21:02:28 +0200

    2

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ def health():
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!! And changed by me!!"
+    return "Hello, World!! And changed by me!"
  
 if __name__ == '__main__':  
    app.run(host="0.0.0.0", port=5000)
